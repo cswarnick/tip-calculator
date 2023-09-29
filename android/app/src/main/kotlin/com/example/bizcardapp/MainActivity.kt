3bfd1aedc4d26b4154cf11e2939378368acfd821
Connor Warnick <cwarnick@ess.engsoftsolutions.local>
2023-09-29 13:36:37 -0400

    Adding project to GitHub repo

---
+++ android/app/src/main/kotlin/com/example/bizcardapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bizcardapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'ui/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BillSplitter(),
+  ));
+}
+
+// void main() {
+//   runApp(const ScaffoldExample());
+// }
+
+// void main() {
+//   runApp(new MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: BizCard(),
+//   ));
+// }
+
+// void main() {
+//   runApp(new MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Wisdom(),
+//   ));
+// }
